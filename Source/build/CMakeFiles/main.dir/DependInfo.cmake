35e9b0167448446b524d02d83910bda02f3b8ce9
Thuc Nguyen <zthucnguyen99@gmail.com>
2020-05-26 15:48:12 +0700

    reset master

---
+++ Source/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/src/Circle.cpp" "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/build/CMakeFiles/main.dir/src/Circle.cpp.o"
+  "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/src/Color.cpp" "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/build/CMakeFiles/main.dir/src/Color.cpp.o"
+  "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/src/DrawManager.cpp" "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/build/CMakeFiles/main.dir/src/DrawManager.cpp.o"
+  "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/src/Ellipse.cpp" "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/build/CMakeFiles/main.dir/src/Ellipse.cpp.o"
+  "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/src/Geometry.cpp" "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/build/CMakeFiles/main.dir/src/Geometry.cpp.o"
+  "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/src/Hyperbola.cpp" "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/build/CMakeFiles/main.dir/src/Hyperbola.cpp.o"
+  "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/src/Line.cpp" "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/build/CMakeFiles/main.dir/src/Line.cpp.o"
+  "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/src/MenuManager.cpp" "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/build/CMakeFiles/main.dir/src/MenuManager.cpp.o"
+  "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/src/Parabola.cpp" "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/build/CMakeFiles/main.dir/src/Parabola.cpp.o"
+  "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/src/Point.cpp" "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/build/CMakeFiles/main.dir/src/Point.cpp.o"
+  "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/src/PolygonInfo.cpp" "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/build/CMakeFiles/main.dir/src/PolygonInfo.cpp.o"
+  "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/src/Rectangle.cpp" "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/build/CMakeFiles/main.dir/src/Rectangle.cpp.o"
+  "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/src/RegularPolygon.cpp" "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/build/CMakeFiles/main.dir/src/RegularPolygon.cpp.o"
+  "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/src/Utils.cpp" "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/build/CMakeFiles/main.dir/src/Utils.cpp.o"
+  "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/src/main.cpp" "/home/zkn99/Documents/StudyPlace/HomeWork/CS411/CS411_Lab/Source/build/CMakeFiles/main.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
